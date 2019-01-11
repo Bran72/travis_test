3fc22d7d8890feca358d45ff0cfc82fc0f5d7016
Brandon Leininger <brandon.leininger@etudiant.univ-reims.fr>
2019-01-11 15:46:11 +0100

    Test travis iteration blablabla

---
+++ NewTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brandonleininger
+ * Date: 11/01/2019
+ * Time: 14:11
+ */
+
+use PHPUnit\Framework\TestCase;
+
+require 'src/Facture.php';
+require 'src/FactureLine.php';
+
+
+class testFacture extends TestCase
+{
+    public function testFacturation()
+    {
+
+        $ligne1 = new \FactureLine\FactureLine('iPhone', 1200, 20);
+        $ligne2 = new \FactureLine\FactureLine('MacBook Pro', 2800, 20);
+        $ligne3 = new \FactureLine\FactureLine('iMac', 2500, 20);
+
+        $lignes = [$ligne1, $ligne2, $ligne3];
+        $facture = new Facture\Facture($lignes);
+
+        $this->assertEquals(6200, $facture->valeurTotale());
+
+    }
+}
